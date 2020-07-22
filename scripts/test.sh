644ac5a81106b29837ef04f05a6c6f9c0305e46a
XDong18 <1141191813@qq.com>
2020-07-22 08:18:42 +0800

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=3,4,6,7,8,9
+export CUDA_VISIBLE_DEVICES=1
 python -u inference.py \
   --task=flow \
   --data_root=/data5/bdd100k/images/track/val \
@@ -9,5 +9,5 @@ python -u inference.py \
   --batch_size=1 \
   --workers=16 \
   --flow_format=png \
-  --model_path=model_zoo/hd3fc_chairs_things_kitti-bfa97911.pth \
-  --save_folder=predictions/fc_pre_KT_seg_track_val
+  --model_path=model_zoo/hd3fc_chairs_things_sintel-0be17c83.pth \
+  --save_folder=predictions/fc_pre_Sintel_seg_track_val
