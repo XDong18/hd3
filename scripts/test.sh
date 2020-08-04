a176a0076a34c1705e3bde1cf2559bf40f8697d9
XDong18 <1141191813@qq.com>
2020-08-05 05:36:05 +0800

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,7 @@ export CUDA_VISIBLE_DEVICES=5,6,7,8
 python -u inference.py \
   --task=flow \
   --data_root=/data5/bdd100k/images/track/val \
-  --data_list=lists/seg_track_val_new_3.txt \
+  --data_list=lists/seg_track_val_new_5.txt \
   --encoder=dlaup \
   --decoder=hda \
   --batch_size=16 \
@@ -10,4 +10,4 @@ python -u inference.py \
   --context \
   --flow_format=png \
   --model_path=/shared/xudongliu/code/hd3/checkpoints/ft_fc_MPI_2_r/model_best.pth \
-  --save_folder=predictions/fc_my_Sintel_seg_track_3
+  --save_folder=predictions/fc_my_Sintel_seg_track_5
