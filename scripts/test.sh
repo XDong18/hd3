debabe01cc4c4ffb5e6d1669c578e8de34d30891
XDong18 <1141191813@qq.com>
2020-08-04 20:09:03 +0800

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,13 +1,13 @@
-export CUDA_VISIBLE_DEVICES=5,6,7,8
+export CUDA_VISIBLE_DEVICES=1,2,3,4
 python -u inference.py \
   --task=flow \
-  --data_root=/data5/bdd100k/images/track/val \
-  --data_list=/shared/xudongliu/code/pytorch-liteflownet/lists/seg_track_val_new.txt \
+  --data_root=/shared/xudongliu/MPI \
+  --data_list=lists/MPISintel_test_final_pass.txt \
   --encoder=dlaup \
   --decoder=hda \
   --batch_size=16 \
   --workers=16 \
   --context \
-  --flow_format=png \
-  --model_path=/shared/xudongliu/code/hd3/checkpoints/ft_fc_MPI_2_r/model_best.pth \
-  --save_folder=predictions/fc_pre_Sintel_seg_track_val_my
+  --flow_format=flo \
+  --model_path=model_zoo/hd3fc_chairs_things_sintel-0be17c83.pth \
+  --save_folder=predictions/fc_pre_Sintel
