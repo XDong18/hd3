78c5b68ffa9013fb23f5c8db73bddddf614b4fa5
XDong18 <1141191813@qq.com>
2020-08-05 01:08:33 +0800

    update

--- read_json.py
+++ read_json.py
@@ -12,12 +12,12 @@ image_list = seg_track_dir['images']
 with open(out_fn, 'w') as f:
     for i, image_info in enumerate(image_list):
         video_id = image_info['video_id']
-        if i==len(image_list) - 5:
+        if i==len(image_list) - 3:
             break
-        next_video_id = image_list[i + 5]['video_id']
+        next_video_id = image_list[i + 3]['video_id']
         if video_id == next_video_id:
             image_fn = os.path.join(image_info['file_name'][:17], image_info['file_name'])
-            next_image_info = image_list[i + 5]
+            next_image_info = image_list[i + 3]
             next_iamge_fn = os.path.join(next_image_info['file_name'][:17], next_image_info['file_name'])
             new_line = image_fn + ' ' + next_iamge_fn + '\n'
             f.write(new_line)
