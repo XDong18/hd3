4f84dc41ab8434e92380b0497d69e8efb3e52109
XDong18 <1141191813@qq.com>
2020-08-05 01:10:37 +0800

    update

--- read_json.py
+++ read_json.py
@@ -3,6 +3,7 @@ import os
 
 fn = '/data5/bdd100k/labels/seg_track/seg_track_val_new.json'
 out_fn = 'lists/seg_track_val_new_3.txt'
+gap = 3
 
 with open(fn) as f:
     seg_track_dir = json.load(f)
@@ -12,12 +13,12 @@ image_list = seg_track_dir['images']
 with open(out_fn, 'w') as f:
     for i, image_info in enumerate(image_list):
         video_id = image_info['video_id']
-        if i==len(image_list) - 3:
+        if i==len(image_list) - gap:
             break
-        next_video_id = image_list[i + 3]['video_id']
+        next_video_id = image_list[i + gap]['video_id']
         if video_id == next_video_id:
             image_fn = os.path.join(image_info['file_name'][:17], image_info['file_name'])
-            next_image_info = image_list[i + 3]
+            next_image_info = image_list[i + gap]
             next_iamge_fn = os.path.join(next_image_info['file_name'][:17], next_image_info['file_name'])
             new_line = image_fn + ' ' + next_iamge_fn + '\n'
             f.write(new_line)
