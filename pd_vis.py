30d031ee5b885367b9cffe333e619a76b934c011
XDong18 <1141191813@qq.com>
2020-08-04 09:51:56 +0800

    update

---
+++ pd_vis.py
@@ -0,0 +1,19 @@
+import cv2
+import os
+
+sur = 'pd_mask/bdd-KT-val-new'
+des = 'pd_mask/bdd-KT-val-new-vis'
+
+def main():
+    for video_fn in os.listdir(sur):
+        if not os.path.exists(os.path.join(des, video_fn)):
+            os.makedirs(os.path.join(des, video_fn))
+        video_dir = os.path.join(sur, video_fn)
+        des_video_dir = os.path.join(des, video_fn)
+        for img_fn in os.listdir(video_dir):
+            img = cv2.imread(os.path.join(video_dir, img_fn))
+            img *= 255 / img.max()
+            cv2.imwrite(os.path.join(des_video_dir, img_fn), img)
+
+if __name__ == "__main__":
+    main()
