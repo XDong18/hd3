466b9ed9f9459324657bb1206aedb2df2c746a3d
XDong18 <1141191813@qq.com>
2020-08-04 20:21:52 +0800

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,7 @@ export CUDA_VISIBLE_DEVICES=1,2,3,4
 python -u inference.py \
   --task=flow \
   --data_root=/shared/xudongliu/MPI \
-  --data_list=lists/MPISintel_test_final_pass.txt \
+  --data_list=lists/MPISintel_test_clean_pass.txt \
   --encoder=dlaup \
   --decoder=hda \
   --batch_size=16 \
@@ -10,4 +10,4 @@ python -u inference.py \
   --context \
   --flow_format=flo \
   --model_path=model_zoo/hd3fc_chairs_things_sintel-0be17c83.pth \
-  --save_folder=predictions/fc_pre_Sintel_final
+  --save_folder=predictions/fc_pre_Sintel_clean
