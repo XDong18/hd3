296a29f92123be6eed9df476c3536adb31cb4e09
XDong18 <1141191813@qq.com>
2020-08-05 05:41:17 +0800

    update

--- read_json.py
+++ read_json.py
@@ -2,8 +2,8 @@ import json
 import os
 
 fn = '/data5/bdd100k/labels/seg_track/seg_track_val_new.json'
-out_fn = 'lists/seg_track_val_new_3.txt'
-gap = 3
+out_fn = 'lists/seg_track_val_new_10.txt'
+gap = 10
 
 with open(fn) as f:
     seg_track_dir = json.load(f)
