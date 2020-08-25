386ffa43d1db4f6ce09ed31779f5c9a5afd08cab
XDong18 <1141191813@qq.com>
2020-08-26 02:51:53 +0800

    update

--- scripts/train.sh
+++ scripts/train.sh
@@ -8,7 +8,7 @@ python -u train.py \
   --val_root=/data5/bdd100k/images/track/train \
   --val_list=lists/seg_track_train.txt \
   --task=flow \
-  --base_lr=0.1 \
+  --base_lr=0.01 \
   --pretrain=model_zoo/hd3fc_chairs_things-0b92a7f5.pth \
   --encoder=dlaup \
   --decoder=hda \
@@ -19,4 +19,4 @@ python -u train.py \
   --batch_size_val=4 \
   --visual_freq=20 \
   --save_step=50 \
-  --save_path=checkpoints/seg_track_bdd_0.1_new
+  --save_path=checkpoints/seg_track_bdd_0.01_new
