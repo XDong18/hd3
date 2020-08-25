725617629d7971fd403ff299980406a9affd67b8
XDong18 <1141191813@qq.com>
2020-08-26 00:39:46 +0800

    update

--- hd3model.py
+++ hd3model.py
@@ -21,36 +21,28 @@ class HD3Model(nn.Module):
         self.eval_epe = EndPointError
         self.hd3net = HD3Net(task, encoder, decoder, corr_range, context,
                              self.ds)
+    
+    def extend_map(self, label_map, corr_range, size): 
+        # TODO constrain origin_map
 
-    def flow_warp(self, x, flo):
-        """
-        inverse warp an image/tensor (im2) back to im1, according to the optical flow
-        x: [B, C, H, W] (im2)
-        flo: [B, 2, H, W] flow
-        """
-        B, C, H, W = x.size()
-        # mesh grid
-        xx = torch.arange(0, W).view(1, -1).repeat(H, 1).to(x.device)
-        yy = torch.arange(0, H).view(-1, 1).repeat(1, W).to(x.device)
-        xx = xx.view(1, 1, H, W).repeat(B, 1, 1, 1)
-        yy = yy.view(1, 1, H, W).repeat(B, 1, 1, 1)
-        grid = torch.cat((xx, yy), 1).float()
+        resized_label_map = torch.nn.functional.interpolate(label_map, size, mode='bilinear')
+        # resized_origin_map = torch.nn.functional.interpolate(origin_map, size, mode='bilinear')
 
-        vgrid = grid + flo
+        B, _, H, W = resized_label_map.size()
+        resized_label_map = resized_label_map.squeeze(1)
 
-        # scale grid to [-1,1]
-        vgrid = torch.stack([
-            2.0 * vgrid[:, 0, :, :] / max(W - 1, 1) - 1.0,
-            2.0 * vgrid[:, 1, :, :] / max(H - 1, 1) - 1.0
-        ],
-                            dim=1)
+        out_list = []
+        x_range = list(range(corr_range + 1))[::-1] + [-1 - p for p in range(corr_range)]
+        y_range = list(range(corr_range + 1))[::-1] + [-1 - p for p in range(corr_range)]
 
-        vgrid = vgrid.permute(0, 2, 3, 1)
-        # output = F.grid_sample(x, vgrid, mode='bilinear', padding_mode='border', align_corners=True)
-        output = F.grid_sample(x, vgrid, mode='bilinear', padding_mode='border')
-
-        return output
-
+        for dy in range(y_range):
+            for dx in range(x_range):
+                temp_label_map = torch.zeros((B, H, W), device=label_map.device)
+                temp_label_map[:, dy:, dx:] = resized_label_map[:, :-dy, :-dx]
+                out_list.append(temp_label_map.eq(1).float().unsqueeze(3).to(label_map.device))
+        
+        out = torch.cat(out_list, dim=3).premute(0, 3, 1, 2)
+        return out
 
     def forward(self,
                 img_list,
@@ -81,27 +73,28 @@ class HD3Model(nn.Module):
 
         if get_loss:
             # add flow warp part
-            corr_range = [4, 4, 4, 4, 4]
+            corr_range_list = [4, 4, 4, 4, 4]
             scale_factor = 1 / 2**(7 - len(corr_range))
-            out_vect = resize_dense_vector(result['vect'] * scale_factor,
-                                            img_list[0].shape[2], img_list[0].shape[3])
-
             total_loss = None
-            for sur_map, tar_map in zip(sur_map_list, tar_map_list):
-                sur_map = sur_map.float().requires_grad_()
-                warped_map = self.flow_warp(sur_map, out_vect)
-                if total_loss is None:
-                    total_loss = self.criterion(warped_map, tar_map.float())
-                else:
-                    total_loss += self.criterion(warped_map, tar_map.float())
+
+            for prob_map, corr_range in zip(ms_prob, corr_range_list):
+                # each level loss
+                for tar_map in tar_map_list:
+                    tar_size = (prob_map.size(2), prob_map.size(3))
+                    extended_tar_map = self.extend_map(tar_map.float(), corr_range, tar_size)
+                    if total_loss is None:
+                        total_loss = self.criterion(prob_map, extended_tar_map)
+                    else:
+                        total_loss += self.criterion(prob_map, extended_tar_map)
             
-            if total_loss is None:
-                result['loss'] = total_loss
-            else:
-                result['loss'] = total_loss / instance_num # TODO
+            result['loss'] = total_loss
+            # if total_loss is None:
+            #     result['loss'] = total_loss
+            # else:
+            #     result['loss'] = total_loss / instance_num 
             # result['num'] = instance_num
         
-        if get_instance_iou: # TODO
+        if get_instance_iou: 
             corr_range = [4, 4, 4, 4, 4]
             scale_factor = 1 / 2**(7 - len(corr_range))
             out_vect = resize_dense_vector(result['vect'] * scale_factor,
