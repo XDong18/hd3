5a9104d8449b7d6e8ea55e036b6a8301b7aa4baf
XDong18 <1141191813@qq.com>
2020-08-26 11:44:56 +0800

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,12 +2,12 @@ export CUDA_VISIBLE_DEVICES=5,6,7,8
 python -u inference.py \
   --task=flow \
   --data_root=/data5/bdd100k/images/track/val \
-  --data_list=lists/seg_track_val_new_3.txt \
+  --data_list=/shared/xudongliu/code/pytorch-liteflownet/lists/seg_track_val_new.txt \
   --encoder=dlaup \
   --decoder=hda \
   --batch_size=16 \
   --workers=16 \
   --context \
   --flow_format=png \
-  --model_path=model_zoo/hd3fc_chairs_things-0b92a7f5.pth \
-  --save_folder=predictions/chairs_thngs_gap_3
+  --model_path=/shared/xudongliu/code/semi-flow/hd3/checkpoints/seg_track_bdd_0.001_new_fix/model_latest.pth \
+  --save_folder=predictions/semi_lr_0.001_gap_1_fix
