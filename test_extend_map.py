324b3b8d2881bfbf65cce7f9bc8bbb2547d46a31
XDong18 <1141191813@qq.com>
2020-08-26 11:28:12 +0800

    update

--- test_extend_map.py
+++ test_extend_map.py
@@ -39,7 +39,7 @@ def extend_map(label_map, corr_range, size):
 def main():
     file_name = '/shared/xudongliu/code/semi-flow/mask/b1c81faa-3df17267/b1c81faa-3df17267-0000001.png'
     np_img = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
-    # print(np_img.shape)
+    print(np_img.shape)
     tensor_img = torch.from_numpy(np_img).unsqueeze(0).unsqueeze(0)
     out = extend_map(tensor_img.float(), 1, np_img.shape)
     np_out = out.squeeze(0).permute(1,2,0).numpy()
