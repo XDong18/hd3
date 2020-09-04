9f3da9205dadc0d0cf1246fc79dc0e54dfce6097
XDong18 <1141191813@qq.com>
2020-09-04 10:39:21 +0800

    update

---
+++ scripts/no_pre_train.sh
@@ -0,0 +1,21 @@
+export CUDA_VISIBLE_DEVICES=1,2,3,4
+python -u train.py \
+  --dataset_name=KITTI \
+  --train_root=/data5/bdd100k/images/track/train \
+  --train_list=lists/seg_track_train.txt \
+  --train_coco=seg_track_train_new.json \
+  --val_coco=/data5/bdd100k/labels/seg_track/seg_track_val_new.json \
+  --val_root=/data5/bdd100k/images/track/train \
+  --val_list=lists/seg_track_train.txt \
+  --task=flow \
+  --base_lr=1e-3 \
+  --encoder=dlaup \
+  --decoder=hda \
+  --context \
+  --workers=16 \
+  --epochs=200 \
+  --batch_size=16 \
+  --batch_size_val=4 \
+  --visual_freq=20 \
+  --save_step=50 \
+  --save_path=checkpoints/seg_track_bdd_1e-3_me_bce_-1_nopre
