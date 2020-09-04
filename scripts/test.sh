f6175f4a4543df3381baa0472c80432c16f6288a
XDong18 <1141191813@qq.com>
2020-09-04 11:53:35 +0800

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=1,2,3,4
+export CUDA_VISIBLE_DEVICES=5,6,7,8
 python -u inference.py \
   --task=flow \
   --data_root=/shared/xudongliu/bdd_part/val \
@@ -9,14 +9,14 @@ python -u inference.py \
   --workers=16 \
   --context \
   --flow_format=png \
-  --model_path=/shared/xudongliu/code/semi-flow/hd3/checkpoints/seg_track_bdd_1e-3_me_bce_-1/model_latest.pth \
-  --save_folder=predictions/seg_track_bdd_1e-3_me_bce_-1_epoch5
+  --model_path=/shared/xudongliu/code/semi-flow/hd3/checkpoints/seg_track_bdd_1e-3_me_bce_-1_nopre/model_latest.pth \
+  --save_folder=predictions/seg_track_bdd_1e-3_me_bce_-1_nopre_epoch9
 
 python color_mask.py \
-  --fl_base=predictions/seg_track_bdd_1e-3_me_bce_-1_epoch5/vec \
-  --out_dir=generated_color_map/seg_track_bdd_1e-3_me_bce_-1_epoch5/frame_0
+  --fl_base=predictions/seg_track_bdd_1e-3_me_bce_-1_nopre_epoch9/vec \
+  --out_dir=generated_color_map/seg_track_bdd_1e-3_me_bce_-1_nopre_epoch9/frame_0
 
 python instance_iou.py \
-  --fl_base=predictions/seg_track_bdd_1e-3_me_bce_-1_epoch5/vec
+  --fl_base=predictions/seg_track_bdd_1e-3_me_bce_-1_nopre_epoch9/vec
 
 
